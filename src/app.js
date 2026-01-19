45939d0c62def6d599b41e96a25ceb52f1527740
chandravan <chandravankumar5145@gmail.com>
2026-01-19 05:37:53 +0530

    fronted url added

--- src/app.js
+++ src/app.js
@@ -1,18 +1,32 @@
-import express, { Router } from "express";
+import express from "express";
 import cors from "cors";
-//import authController from "./controller/authController.js";
 import router from "./routes/apiRouter.js";
 import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
+
+// Allowed origins
+const allowedOrigins = [
+  "https://qr-scan-frontend.vercel.app",       // frontend
+  "https://qr-admin-frontend-six.vercel.app"   // admin panel
+];
+
 app.use(cors({
-    origin: "*", // Aapke React ka URL
-                   // Taaki cookies (auth) kaam kar sakein
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true); // for Postman or server-to-server
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = `CORS policy: Access denied from ${origin}`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true // cookies & auth headers
 }));
 
-app.use(express.json())
+app.use(express.json());
 app.use(cookieParser());
 
-//route
-app.use("/api",router);
-export default app
+// API routes
+app.use("/api", router);
+
+export default app;
